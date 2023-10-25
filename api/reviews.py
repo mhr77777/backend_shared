2922d3ad8c3f77ac5b6ac663ea8c32b3253dafb5
nikkihekmat <nikkihekmat@gmail.com>
2023-10-25 14:03:35 -0700

    hi

---
+++ api/reviews.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify, Blueprint
+import time
+import math
+import json
+import requests
+import asyncio
+import threading
+
+apigenres = Blueprint("genres",__name__,url_prefix='/api/genres')
+
+app = Flask(__name__)
+uTime = float(time.time() + 30.0)
+apidata = {}
+#Hopefully this is more secure than the old code lol
+sbkey = open(".dbkeys").read().split("=")[1]
+def UpdateAPI():
+    reviewsdat = requests.get("https://jcvfukpccvibxumakqdh.supabase.co/rest/v1/genres?select=*",headers={"apikey":sbkey,"Authorization":"Bearer "+sbkey})
+    reviewsdat = reviewsdat.json()
+    return genresdat
+@apigenres.route('/', methods=['GET'])
+def get_items():
+    apidata = UpdateAPI()
+    return jsonify(apidata)
+
+# GET endpoint to retrieve a specific item by ID
+@apigenres.route('/item_id=<int:item_id>', methods=['GET'])
+def get_item(item_id):
+    item = next((item for item in apidata if item["id"] == item_id), None)
+    if item is None:
+        return jsonify({"error": "Item not found"}), 404
+    return jsonify(item)
+
+if __name__ == '__main__':
+    app.run(debug=True)
