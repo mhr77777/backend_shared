20840e2074826c7a19f99ba104b6622387f0fa3b
nikkihekmat <nikkihekmat@gmail.com>
2023-10-25 14:08:27 -0700

    hi

--- api/reviews.py
+++ api/reviews.py
@@ -25,6 +25,7 @@ def get_items():
 # GET endpoint to retrieve a specific item by ID
 @apireviews.route('/item_id=<int:item_id>', methods=['GET'])
 def get_item(item_id):
+    apidata = UpdateAPI()
     item = next((item for item in apidata if item["id"] == item_id), None)
     if item is None:
         return jsonify({"error": "Item not found"}), 404
