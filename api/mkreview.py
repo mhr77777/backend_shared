252ee700050ec02922fec7ec02bdc9628218d3f0
mhr77777 <142542308+mhr77777@users.noreply.github.com>
2023-11-02 13:38:29 -0700

    WORK PLEASE

--- api/mkreview.py
+++ api/mkreview.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, Blueprint, request
+from flask import Flask, jsonify, Blueprint, Request
 import time
 import math
 import json
@@ -30,7 +30,7 @@ def getLatestID():
 @apireview.route('/', methods=['POST'])
 def post_review():
 
-    reqdat = request.json()
+    reqdat = Request.json()
     try:
         rvat = requests.post("https://jcvfukpccvibxumakqdh.supabase.co/rest/v1/reviews",
                   headers={
